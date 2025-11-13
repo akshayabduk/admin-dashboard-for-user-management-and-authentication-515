17fe17bea40cbf7df8601ea7036aa0c527447194
Kavia AI <production@kavia.ai>
2025-11-13 10:55:50 +0000

    Checkpoint 1 - cg72cedc34

---
+++ fido2_backend/src/main/java/com/example/demo/fido2backendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fido2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class fido2backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(fido2backendApplication.class, args);
+	}
+
+}
